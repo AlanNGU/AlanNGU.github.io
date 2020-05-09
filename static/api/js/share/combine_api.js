e9a143da8389dc7555fe0e64df5a7c0785d805a1
xiexiyu <2723916809@qq.com>
2020-05-09 11:53:35 +0800

    Site updated: 2020-05-09 11:53:33

--- static/api/js/share/combine_api.js
+++ static/api/js/share/combine_api.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 09 2020 16:42:37 GMT+0800 (GMT+08:00)
+// build time:Sat May 09 2020 11:53:16 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=(n("base/tangram").T,n("base/class").Class),s=n("share/api_base");a.CombineApi=e.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},s.ApiBase)});
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 09 2020 16:42:37 GMT+0800 (GMT+08:00)
+// build time:Sat May 09 2020 11:53:16 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 

b79f9e041c1b6250fdee021dffa702b09676b94f
xiexiyu <2723916809@qq.com>
2020-05-26 17:58:23 +0800

    Site updated: 2020-05-26 17:58:19

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon May 25 2020 17:43:55 GMT+0800 (GMT+08:00)
+// build time:Tue May 26 2020 17:56:41 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]==i&&$(window).scrollTop(e[0])}});
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Mon May 25 2020 17:43:56 GMT+0800 (GMT+08:00)
+// build time:Tue May 26 2020 17:56:43 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 

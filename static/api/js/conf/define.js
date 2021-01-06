144d0df3443d631efabc189189c85a71e9a2f58f
xiexiyu <2723916809@qq.com>
2021-01-06 16:54:34 +0800

    Site updated: 2021-01-06 16:54:33

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Fri May 29 2020 10:28:59 GMT+0800 (GMT+08:00)
+// build time:Wed Jan 06 2021 16:54:23 GMT+0800 (GMT+08:00)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Fri May 29 2020 10:28:59 GMT+0800 (GMT+08:00)
+// build time:Wed Jan 06 2021 16:54:23 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
